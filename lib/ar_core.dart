9433b6fdd5b5a4342dcc352e359597a1b4a2f5a3
Arslan  Raza <arslan.raza@bluehorntech.co.uk>
2022-03-15 18:31:55 +0500

    Complete Augmented Reality Solution

--- lib/ar_core.dart
+++ lib/ar_core.dart
@@ -1,7 +1,100 @@
-library flutter_package;
+import 'package:flutter/material.dart';
+import 'package:camera/camera.dart';
 
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
+import 'augmented_reality/camera_preview.dart';
+
+
+
+class Augmented extends StatefulWidget {
+
+  @override
+  _AugmentedState createState() => _AugmentedState();
 }
+
+class _AugmentedState extends State<Augmented> {
+  var controller;
+  List<CameraDescription> cameras = [];
+  bool loadingCamera = false;
+  void loadCamera() async {
+    setState(() {
+      loadingCamera = true;
+    });
+    try {
+      cameras = await availableCameras();
+      controller = CameraController(cameras[0], ResolutionPreset.ultraHigh);
+      await controller.initialize();
+      loadingCamera = false;
+
+      print('controller value');
+      print(controller.toString());
+    } catch (e) {
+      loadingCamera = false;
+      print(e.toString());
+    }
+
+    /// Timer
+
+  }
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    loadCamera();
+  }
+  double xPosition = 0;
+  double yPosition = 0;
+  double height = 150;
+  double onchange = 22;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Testing'),
+      ),
+      body: Stack(
+        children: [
+          Container(
+              height: 1000,
+              width: MediaQuery.of(context).size.width,
+              child: CameraWidget(controller)),
+          Positioned(
+            top: yPosition,
+            left: xPosition,
+            child: GestureDetector(
+
+
+              onPanUpdate: (tapInfo) {
+                setState(() {
+
+                  xPosition += tapInfo.delta.dx;
+                  yPosition += tapInfo.delta.dy;
+                });
+              },
+              child:
+              Container(
+                  height: onchange,
+                  color: Colors.transparent,
+                  child: Image.asset('assets/female.png', height: onchange,width: onchange,)),
+            ),
+          ),
+          Positioned(
+              bottom: 0,
+              left: 0,
+              child: Slider(
+                value: onchange,
+                min: 20,
+                max: 100,
+                onChanged: (value){
+                  setState(() {
+                    onchange = value;
+                  });
+                },
+              )
+          )
+        ],
+      ),
+    );
+  }
+}
+
---
+++ lib/augmented_reality/camera_preview.dart
@@ -0,0 +1,93 @@
+
+import 'dart:io';
+
+import 'package:camera/camera.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+/// A widget showing a live camera preview.
+class CameraPreview extends StatelessWidget {
+  /// Creates a preview widget for the given camera controller.
+  const CameraPreview(this.controller, {this.child});
+
+  /// The controller for the camera that the preview is shown for.
+  final CameraController controller;
+
+  /// A widget to overlay on top of the camera preview
+  final Widget? child;
+
+  @override
+  Widget build(BuildContext context) {
+    return controller.value.isInitialized
+        ? AspectRatio(
+      aspectRatio: _isLandscape()
+          ? controller.value.aspectRatio
+          : (1 / controller.value.aspectRatio),
+      child: Stack(
+        fit: StackFit.expand,
+        children: [
+          _wrapInRotatedBox(child: controller.buildPreview()),
+          child ?? Container(),
+        ],
+      ),
+    )
+        : Container();
+  }
+
+  Widget _wrapInRotatedBox({required Widget child}) {
+    if (defaultTargetPlatform != TargetPlatform.android) {
+      return child;
+    }
+
+    return RotatedBox(
+      quarterTurns: _getQuarterTurns(),
+      child: child,
+    );
+  }
+
+  bool _isLandscape() {
+    return [DeviceOrientation.landscapeLeft, DeviceOrientation.landscapeRight]
+        .contains(_getApplicableOrientation());
+  }
+
+  int _getQuarterTurns() {
+    Map<DeviceOrientation, int> turns = {
+      DeviceOrientation.portraitUp: 0,
+      DeviceOrientation.landscapeLeft: 1,
+      DeviceOrientation.portraitDown: 2,
+      DeviceOrientation.landscapeRight: 3,
+    };
+    return turns[_getApplicableOrientation()]!;
+  }
+
+  DeviceOrientation _getApplicableOrientation() {
+    return controller.value.isRecordingVideo
+        ? controller.value.recordingOrientation!
+        : (controller.value.lockedCaptureOrientation ??
+        controller.value.deviceOrientation);
+  }
+}
+
+
+class CameraWidget extends StatelessWidget {
+  CameraWidget(this.controller);
+  final controller;
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(children: [
+      Container(
+        width: double.infinity,
+        height: 1000,
+        decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(10)
+        ),
+        child:
+        CameraPreview(controller),
+      ),
+
+
+    ]);
+  }
+}
